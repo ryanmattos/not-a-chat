1e51023e4abe4c905ce776ebe63fa68c2111557d
ryan.matos <ryan.matos@itixti.com.br>
2021-12-03 17:23:20 -0300

    t

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
 import { server } from "./http"
 import './websocket'
 
-server.listen(process.env.PORT || 3000, () => console.log('Server is running:'))
+server.listen(process.env.PORT || 3000, () => console.log('Server is running'))
